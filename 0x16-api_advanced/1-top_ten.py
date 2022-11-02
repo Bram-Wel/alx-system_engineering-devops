36890e32dba0174a991ea16ea57b32e02720aaac
Bram-Wel <welbram@gmail.com>
2022-11-02 05:19:14 +0300

    Rest API reddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,31 @@
+
+#!/usr/bin/python3
+"""This module describes a query freturn."""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Query the Reddit API.
+
+    Args:
+        @subredit: The type of resource/object requested
+    Return:
+        The number of subscribers for the subreddit, or 0 if
+    """
+    headers = {'user-agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"'}
+    url = f"https://api.reddit.com/r/{subreddit}/about"
+    resp = requests.get(url, headers, allow_redirects=False)
+
+    try:
+        resp.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        # print(e)
+        return 0
+    else:
+        try:
+            number = resp.json().get('data')['subscribers']
+        except requests.exceptions.JSONDecodeError as e:
+            # print(e)
+            return 0
+        return number

3ad9817126dab3c6908ea925df143a3c3b1cb26d
Bram-Wel <welbram@gmail.com>
2022-11-02 04:23:56 +0300

    Rest API reddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module describes a query freturn."""
+
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Query the Reddit API.
+
+    Args:
+        @subredit: The type of resource/object requested
+    Return:
+        The number of subscribers for the subreddit, or 0 if
+    """
+    resp = requests.get(f"https://api.reddit.com/r/{subreddit}/about", headers={'user-agent': 'Bram'}, allow_redirects=False)
+
+    try:
+        resp.raise_for_status()
+    except requests.exceptions.HTTPError:
+        return 0
+    else:
+        try:
+            number = resp.json().get('data')['subscribers']
+        except requests.exceptions.JSONDecodeError:
+            return 0
+        return resp.json().get('data')['subscribers']
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,4 @@
+# REDDIT API
+
+## Adavanced API, Python, Scripting, Back-end
+[![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/)

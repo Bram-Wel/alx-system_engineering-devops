5537af296b6fbdb345ff834f1c8fc1b233152310
Bram-Wel <welbram@gmail.com>
2022-11-02 05:57:43 +0300

    Rest API reddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,31 +1,31 @@
-
 #!/usr/bin/python3
-"""This module describes a query freturn."""
-
+"""script for parsing web data from an api
+"""
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """Query the Reddit API.
-
-    Args:
-        @subredit: The type of resource/object requested
-    Return:
-        The number of subscribers for the subreddit, or 0 if
+def top_ten(subreddit):
+    """api call to reddit to get the number of subscribers
     """
-    headers = {'user-agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}
-    url = f"https://api.reddit.com/r/{subreddit}/about"
-    resp = requests.get(url, headers, allow_redirects=False)
+    base_url = 'https://www.reddit.com/r/'
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+         (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+    }
+    url = base_url + '{}/top/.json?count=10'.format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    resp = json.loads(response.text)
 
     try:
-        resp.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-        # print(e)
-        return 0
-    else:
-        try:
-            number = resp.json().get('data')['subscribers']
-        except requests.exceptions.JSONDecodeError as e:
-            # print(e)
-            return 0
-        return number
+        data = resp.get('data')
+        children = data.get('children')
+    except Exception as e:
+        print('None')
+    if children is None or data is None or len(children) < 1:
+        print('None')
+
+    for i, post_dict in enumerate(children):
+        if i == 10:
+            break
+        print(post_dict.get('data').get('title'))

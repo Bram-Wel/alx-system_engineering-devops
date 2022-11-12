3223c85bda2e73c54f0b06e6dbd027c7d96737c5
Bram-Wel <welbram@gmail.com>
2022-11-12 19:48:23 +0300

    Puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,20 @@
-# Debug web open files per user
-exec { 'fix--for-nginx':
-  environment => ['DIR=/etc/default/nginx',
-                  'OLD=ULIMIT="-n 15"',
-                  'NEW=ULIMIT="-n 15000"'],
-  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo service nginx restart',
-  path        => ['/usr/bin', '/bin'],
-  returns     => [0, 1]
-}i
+# puppet fix for allowing multiple worker processes
+$new_limit = 'worker_rlimit_nofile 30000;'
+$replace = "sed -i '/user www-data/a ${::new_limit}' /etc/nginx/nginx.conf"
+$replace_exists = "grep \"${::new_limit}\" /etc/nginx/nginx.conf"
+exec { 'nofile_fix' :
+  path    => '/bin',
+  unless  => $replace_exists,
+  command => $replace,
+}
+exec { 'hard_stop_nginx' :
+  path    => '/usr/bin',
+  onlyif  => 'pgrep nginx',
+  command => 'pkill nginx',
+}
+
+exec { 'nginx_start' :
+  unless    => '/usr/bin/pgrep nginx',
+  subscribe => Exec['hard_stop_nginx'],
+  command   => '/usr/sbin/nginx'
+}

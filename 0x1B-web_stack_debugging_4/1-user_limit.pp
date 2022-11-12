db68eef7250cc974c3119730ddb1bb1ed5d5ca7a
Bram-Wel <welbram@gmail.com>
2022-11-12 14:21:54 +0300

    Puppet

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,32 @@
-# Debug increase limit files per user
-exec { 'change-os-configuration-for-holberton-user':
-  environment => ['DIR=/etc/security/limits.conf',
-                  'OLD=hard nofile 5',
-                  'NEW=hard nofile 50000',
-                  'OLD2=soft nofile 4',
-                  'NEW2=soft nofile 40000'],
-  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo sed -i "s/$OLD2/$NEW2/" $DIR',
-  path        => ['/usr/bin', '/bin'],
-  returns     => [0, 1]
+# puppet fix for allowing higher hard/soft file open limits
+$file = '/etc/security/limits.conf'
+# setup search and replace strings
+$hard_txt = 'holberton hard nofile'
+$hard_rep = "${::hard_txt} 10000"
+$soft_txt = 'holberton soft nofile'
+$soft_rep = "${::soft_txt} 8000"
+# verify text hasn't been replaced already
+$hard_exists = "grep ${::hard_rep} ${::file}"
+$soft_exists = "grep ${::soft_rep} ${::file}"
+# replace the text
+$hard_cmd = "sed -i '/${::hard_txt} /c ${::hard_rep}' ${::file}"
+$soft_cmd = "sed -i '/${::soft_txt} /c ${::soft_rep}' ${::file}"
+
+# actual execution / puppet logic
+exec { 'hard_limit_fix' :
+  path    => '/bin',
+  unless  => $hard_exists,
+  command => $hard_cmd,
 }
-© 2020 GitHub, Inc.i
+exec { 'soft_limit_fix' :
+  path    => '/bin',
+  unless  => $soft_exists,
+  command => $soft_cmd,
+}
+
+# restart sysctl after applying changes to limits file
+exec { 'sysctl_restart' :
+  path      => '/sbin',
+  subscribe => Exec['soft_limit_fix', 'hard_limit_fix'],
+  command   => 'sysctl -p',
+}

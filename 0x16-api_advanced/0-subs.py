a444489cbecae8d6a0afb629c2f91f202926daa9
Bram-Wel <welbram@gmail.com>
2023-02-01 17:41:23 +0300

    Revert "No. of subscribers"
    This reverts commit 974e3ec34da93c23eb3239154598088b3378484d.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,19 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Query the Reddit API"""
+    """Query the Reddit API.
+
+    Args:
+        @subredit: The type of resource/object requested
+    Return:
+        The number of subscribers for the subreddit, or 0 if
+    """
     user_agent = "android:com.example.myredditapp:v1.2.3 (by /u/kemitche)"
+    # print(user_agent)
     headers = {'user-agent': user_agent}
     url = f"https://api.reddit.com/r/{subreddit}/about"
     resp = requests.get(url, headers=headers, allow_redirects=False)
+
     try:
         resp.raise_for_status()
     except requests.exceptions.HTTPError as e:
@@ -19,8 +27,7 @@ def number_of_subscribers(subreddit):
     else:
         try:
             number = resp.json().get('data')['subscribers']
-        # except requests.exceptions.JSONDecodeError as e:
-        except Exception:
+        except requests.exceptions.JSONDecodeError as e:
             # print(e)
             return 0
         return number

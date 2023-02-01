a8a4fa9c381b25341ec0776c47d0b97cbee9970d
Bram-Wel <welbram@gmail.com>
2023-02-01 17:13:36 +0300

    No. of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,33 +1,16 @@
 #!/usr/bin/python3
-"""This module describes a query freturn."""
-
+"""
+Contains function number_of_subscribers
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Query the Reddit API.
-
-    Args:
-        @subredit: The type of resource/object requested
-    Return:
-        The number of subscribers for the subreddit, or 0 if
-    """
-    user_agent = "User-Agent: android:com.example.myredditapp:v1.2.3\
- (by /u/kemitche)"
-    print(user_agent)
-    headers = {'user-agent': user_agent}
-    url = f"https://api.reddit.com/r/{subreddit}/about"
-    resp = requests.get(url, headers=headers, allow_redirects=False)
-
-    try:
-        resp.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-        print(e)
+    """Returns the number of subscribers of a subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': "My user agent 1.0"}
+    subreddit_data = requests.get(url, allow_redirects=False, headers=headers)
+    if subreddit_data.status_code == 404:
         return 0
-    else:
-        try:
-            number = resp.json().get('data')['subscribers']
-        except requests.exceptions.JSONDecodeError as e:
-            # print(e)
-            return 0
-        return number
+    number_of_subscribers = subreddit_data.json()['data']['subscribers']
+    return number_of_subscribers

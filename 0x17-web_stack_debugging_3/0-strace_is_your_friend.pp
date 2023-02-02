e2aa37f019f27b18742bc1edb3d5478dc99b1ed1
Bram-Wel <welbram@gmail.com>
2023-02-02 12:27:40 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,6 @@
 # Correct misspelling on /var/www/html/wp-settings.php line 137
 
-include stdlib
-
-file_line {'/var/www/html/wp-settings.php':
-  ensure => present,
-  path   => '/var/www/html/wp-settings.php',
-  line   => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );",
-  match  => '^require_once\( ABSPATH \. WPINC \. \'\/class-wp-locale\.phpp\' \);$',
+exec { 'fix-typo':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
